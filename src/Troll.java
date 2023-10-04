2ba3bc7d145dad7d1104ed3bcdd0627e42807a4f
Eric Cristelli <eric.cristelli@buonarroti.tn.it>
2023-10-04 19:02:18 +0200

    feat: aggiunti getter

--- src/Troll.java
+++ src/Troll.java
@@ -1,5 +1,29 @@
+import java.util.Objects;
+
 public class Troll {
     private String nome;
     private int puntiFerita;
+
+    public Troll(String nome, int puntiFerita)throws Exception{
+        if(puntiFerita>= 0){
+            if(nome != null && !nome.equals("")){
+                this.puntiFerita = puntiFerita;
+                this.nome = nome;
+            }else{
+                throw new Exception("Nome errato");
+            }
+
+        }else{
+            throw new Exception("Punti ferita non validi");
+        }
+    }
+
+    public int getPuntiFerita() {
+        return puntiFerita;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 
 }
